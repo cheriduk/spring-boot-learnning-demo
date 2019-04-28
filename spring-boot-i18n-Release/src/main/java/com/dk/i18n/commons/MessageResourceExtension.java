43b7362b85b8f7a4f0ebf98195e0c2e4550183a5
cheriduk <1227826445@qq.com>
2019-04-28 23:12:25 +0800

    springboot国际化配置的学习案例 1.修改了jar下无法访问国际化配置文件的问题

--- spring-boot-i18n-Release/src/main/java/com/dk/i18n/commons/MessageResourceExtension.java
+++ spring-boot-i18n-Release/src/main/java/com/dk/i18n/commons/MessageResourceExtension.java
@@ -17,10 +17,16 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.jar.JarFile;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
 
 /**
  * @title: MessageResourceExtension
@@ -149,6 +155,58 @@ public class MessageResourceExtension extends ResourceBundleMessageSource {
         }
         System.out.println(ColourAnsiUtil.YELLOW+"国际化文件名filename:=>"+filename);
         return filename.replace("\\", "/");
+    }
+
+
+
+
+    /**
+     * 获取文件夹下所有的国际化文件名 (优化版,修改了jar下无法访问配置文件的问题)
+     */
+    private String[] getAllBaseNamesPro(final String folderName) throws IOException {
+        URL url = Thread.currentThread().getContextClassLoader()
+                .getResource(folderName);
+        if (null == url) {
+            throw new RuntimeException("无法获取资源文件路径");
+        }
+
+        List<String> baseNames = new ArrayList<>();
+        if (url.getProtocol().equalsIgnoreCase("file")) {
+            // 文件夹形式,用File获取资源路径
+            File file = new File(url.getFile());
+            if (file.exists() && file.isDirectory()) {
+                baseNames = Files.walk(file.toPath())
+                        .filter(path -> path.toFile().isFile())
+                        .map(Path::toString)
+                        .map(path -> path.substring(path.indexOf(folderName)))
+                        .map(this::getI18FileName)
+                        .distinct()
+                        .collect(Collectors.toList());
+            } else {
+                logger.error("指定的baseFile不存在或者不是文件夹");
+            }
+        } else if (url.getProtocol().equalsIgnoreCase("jar")) {
+            // jar包形式，用JarEntry获取资源路径
+            String jarPath = url.getFile().substring(url.getFile().indexOf(":") + 2, url.getFile().indexOf("!"));
+            JarFile jarFile = new JarFile(new File(jarPath));
+            List<String> baseJars = jarFile.stream()
+                    .map(ZipEntry::toString)
+                    .filter(jar -> jar.endsWith(folderName + "/")).collect(Collectors.toList());
+            if (baseJars.isEmpty()) {
+                logger.info("不存在{}资源文件夹", folderName);
+                return new String[0];
+            }
+
+            baseNames = jarFile.stream().map(ZipEntry::toString)
+                    .filter(jar -> baseJars.stream().anyMatch(jar::startsWith))
+                    .filter(jar -> jar.endsWith(".properties"))
+                    .map(jar -> jar.substring(jar.indexOf(folderName)))
+                    .map(this::getI18FileName)
+                    .distinct()
+                    .collect(Collectors.toList());
+
+        }
+        return baseNames.toArray(new String[0]);
     }
 
 }
